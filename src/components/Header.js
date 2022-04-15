0c402d521d49601ab2e27907532fd2b5dc265295
gokulsellappan <gokulsellappan@gmail.com>
2022-04-15 10:16:00 +0530

    Header change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Header() {
   return (
-    <div>Header</div>
+    <Container>
+      <a>
+        <img src='./images/logo.svg'/>
+      </a>
+      <Menu>
+
+      </Menu>
+    </Container>
   )
 }
 
 export default Header
+
+const Container=styled.div`
+    min-height:60px;
+    position:fixed;
+    display:flex;
+    padding:0 20px;
+    align-items:center;
+
+`
+const Menu=styled.div`
+`
